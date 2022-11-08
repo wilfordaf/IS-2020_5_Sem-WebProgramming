f7b5b28a8794527a2b1855eea55f8753c871f062
Sergey <79761555+wilfordaf@users.noreply.github.com>
2022-11-08 16:28:55 +0300

    Initial commit lab-4 and lab-5

---
+++ scripts/formControl.js
@@ -0,0 +1,86 @@
+window.addEventListener("load", () => {
+    updateUI()
+})
+
+document.querySelector(".form").addEventListener("submit", (e) => {
+    e.preventDefault()
+    let input = document.querySelectorAll(".form__input-text")
+    const inputData = Object.values(input).map(function (e) {
+        return e.value
+    })
+    if (inputData.includes("")) {
+        alert("⛔ Incorrect input!");
+        return
+    }
+
+    const item = createQuestion(...inputData)
+    addQuestion(item)
+});
+
+function getQuestionsList() {
+    return JSON.parse(localStorage.getItem("questionList")) || []
+}
+
+function addQuestion(item) {
+    const questions = getQuestionsList()
+    questions.unshift(item)
+    updateStorage(questions)
+    updateUI()
+}
+
+function removeQuestion(item) {
+    let questions = getQuestionsList()
+    questions = questions.filter(q => JSON.stringify(q) !== JSON.stringify(item))
+    updateStorage(questions)
+}
+
+function updateStorage(questions) {
+    localStorage.setItem("questionList", JSON.stringify(questions))
+}
+
+function updateUI() {
+    const questions = getQuestionsList()
+    const container = document.querySelector(".content-qna__item-list")
+    container.innerHTML =""
+    
+    Array.prototype.forEach.call(questions, q => {
+        const questionDOM = createQuestionDOM(q.Name, q.Contact, q.Message)
+        const questionNode = createQuestionTemplate(questionDOM)
+        container.appendChild(questionNode)
+    });
+
+    const deleteButtonList = document.querySelectorAll(".item__delete-button")
+    deleteButtonList.forEach((b, index) => {
+        b.addEventListener("click", () => {
+            removeQuestion(questions[index])
+            updateUI()
+        })
+    })
+}
+
+function createQuestion(name, contact, message) {
+    return {
+        Name: name,
+        Contact: contact,
+        Message: message
+    }
+}
+
+function createQuestionDOM(name, contact, message) {
+    return `
+        <div class="content-qna__item item">
+            <div class="item__body">
+                <button class="item__delete-button">X</button>
+                <div class="item__name">Name: ${name}</div>
+                <div class="item__contact-info">Contact: ${contact}</div>
+                <div class="item__text">Message: ${message}</div>
+            </div>
+        </div>`
+}
+
+function createQuestionTemplate(stringContent) {
+    let template = document.createElement('template');
+    stringContent = stringContent.trim();
+    template.innerHTML = stringContent
+    return template.content.firstChild;
+}
